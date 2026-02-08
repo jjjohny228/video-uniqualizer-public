c247e90233fb40136fc2c7f23ead3b11610d934a
jjjohny228 <gggleb13@gmail.com>
2026-02-08 21:33:52 +0200

    Added subtitles function

--- main.py
+++ main.py
@@ -1,11 +1,128 @@
 import os
 import random
+import shutil
 import subprocess
 import time
 from typing import Optional
 from uuid import uuid4
+from openai import OpenAI
 
 from config import Config
+
+
+def extract_audio(input_video: str):
+    # Ensure the output file has a valid audio extension
+    output_audio = f'temp/{uuid4()}.mp3'
+
+    # FFmpeg command to extract audio
+    command = [
+        'ffmpeg',
+        '-i', input_video,
+        '-q:a', '0',
+        '-map', 'a',
+        '-y', output_audio
+    ]
+
+    try:
+        # Run the FFmpeg command
+        subprocess.run(command, check=True)
+        print(f"Audio extracted successfully: {output_audio}")
+        return output_audio
+    except subprocess.CalledProcessError as e:
+        print(f"An error occurred while extracting audio: {e}")
+
+
+def format_time(seconds):
+    hours = int(seconds // 3600)
+    minutes = int((seconds % 3600) // 60)
+    secs = int(seconds % 60)
+    centiseconds = int((seconds - int(seconds)) * 100)
+    return f"{hours:02}:{minutes:02}:{secs:02}.{centiseconds:02}"
+
+
+def subtitles_from_audio(file: str, language_code: str):
+    client = OpenAI()
+    with open(file, 'rb') as media_file:
+        transcript = client.audio.transcriptions.create(
+            model="whisper-1",
+            file=media_file,
+            temperature=0.2,
+            language=language_code,
+            response_format="verbose_json",
+            timestamp_granularities=['word']
+        )
+
+    transcript_dict = transcript.to_dict()  # или transcript.dict()
+    words = transcript_dict.get("words", [])
+
+    ass_header = """[Script Info]
+Title: Generated ASS file
+ScriptType: v4.00+
+
+
+[V4+ Styles]
+Format: Name, Fontname, Fontsize, PrimaryColour, SecondaryColour, OutlineColour, BackColour, Bold, Italic, Underline, StrikeOut, ScaleX, ScaleY, Spacing, Angle, BorderStyle, Outline, Shadow, Alignment, MarginL, MarginR, MarginV, Encoding
+Style: Default,Arial Black,20,&H0000FFFF,&H000000FF,&H00000000,&H00000000,-1,0,0,0,100,100,0,0,1,2,0,5,0,0,0,1
+
+[Events]
+Format: Layer, Start, End, Style, Name, MarginL, MarginR, MarginV, Effect, Text
+"""
+
+    subtitles = [ass_header]
+
+    for word in words:
+        start_time = format_time(word.get('start'))
+        end_time = format_time(word.get('end'))
+        text = word.get('word')
+
+        subtitle_line = f"Dialogue: 0,{start_time},{end_time},Default,,0,0,0,,{text}\n"
+        subtitles.append(subtitle_line)
+
+    ass_file = f'temp/{uuid4()}.txt'
+    with open(ass_file, 'w', encoding='utf-8') as f:
+        f.writelines(subtitles)
+
+    return ass_file
+
+
+def get_audio_duration(audio_path):
+    # Используем ffprobe для получения продолжительности аудио
+    command = [
+        'ffprobe',
+        '-i', audio_path,  # Входной файл аудио
+        '-hide_banner',  # Скрыть ненужную информацию
+        '-loglevel', 'error',  # Скрыть ненужные ошибки
+        '-show_entries', 'format=duration',  # Показать только продолжительность
+        '-of', 'default=noprint_wrappers=1:nokey=1'  # Вывод только значения
+    ]
+
+    result = subprocess.run(command, capture_output=True, text=True)
+
+    # Если успешно получили данные, возвращаем продолжительность в секундах
+    try:
+        duration = float(result.stdout.strip())
+        return duration
+    except ValueError:
+        print("Error: Could not determine audio duration.")
+        return None
+
+
+def add_subtitles_to_video(video_path, subtitles_file):
+    output_file = f'temp/{uuid4()}.mp4'
+
+    command = [
+        'ffmpeg',
+        '-i', video_path,
+        '-vf', f"ass='{subtitles_file}'",
+        '-c:v', Config.VIDEO_CODEC,
+        '-c:a', 'aac',
+        '-pix_fmt', 'yuv420p',
+        '-y',
+        output_file
+    ]
+
+    subprocess.run(command)
+    return output_file
 
 
 def get_video_duration(video_path):
@@ -80,9 +197,11 @@ def combine_videos(source_video, background_video, target_folder):
     # Запускаем команду
     subprocess.run(command)
     os.remove(trimmed_bottom_video)
+    return output_video
 
 
 if __name__ == '__main__':
+    ADD_SUBTITLES = True
     source_video_folder = 'source_videos'
     result_video_folder = 'result_videos'
     bottom_video_folder = 'bottom_videos'
@@ -93,5 +212,12 @@ if __name__ == '__main__':
     for video in source_videos:
         full_video_path = os.path.join(source_video_folder, video)
         random_bottom_video_path = os.path.join(bottom_video_folder, random.choice(bottom_videos))
-        combine_videos(full_video_path, random_bottom_video_path, Config.RESULT_FOLDER)
+        combined_video = combine_videos(full_video_path, random_bottom_video_path, Config.RESULT_FOLDER)
+        if ADD_SUBTITLES:
+            audio_from_video = extract_audio(combined_video)
+            subtitles_file = subtitles_from_audio(audio_from_video, 'en')
+            result = add_subtitles_to_video(combined_video, subtitles_file)
+            shutil.move(result, os.path.join(result_video_folder, f'{int(time.time())}.mp4'))
+            os.remove(audio_from_video)
+            os.remove(subtitles_file)
         os.remove(full_video_path)
